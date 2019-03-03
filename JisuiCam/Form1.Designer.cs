72205970ae872e47493e12e56111fa5ed118013c
gummoni <shaty3@hotmail.com>
2019-03-03 16:09:21 +0900

    プロジェクト ファイルを追加します。

---
+++ JisuiCam/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace JisuiCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ButtonCapture = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.ButtonCapture);
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "R:\\book";
+            // 
+            // ButtonCapture
+            // 
+            this.ButtonCapture.Location = new System.Drawing.Point(117, 12);
+            this.ButtonCapture.Name = "ButtonCapture";
+            this.ButtonCapture.Size = new System.Drawing.Size(99, 49);
+            this.ButtonCapture.TabIndex = 1;
+            this.ButtonCapture.Text = "撮影";
+            this.ButtonCapture.UseVisualStyleBackColor = true;
+            this.ButtonCapture.Click += new System.EventHandler(this.ButtonCapture_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(99, 49);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "開始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 384);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ButtonCapture;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ JisuiCam/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace JisuiCam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCapture video = null;
+        int counter = 1;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //開始
+            if (buttonStart.Text == "開始")
+            {
+                try
+                {
+                    video = new VideoCapture();
+                    video.Open(0);
+                    video.FrameWidth = 1920;
+                    video.FrameHeight = 1080;
+
+                    if (!video.IsOpened())
+                    {
+                        MessageBox.Show("カメラがありません");
+                        return;
+                    }
+
+                    buttonStart.Text = "停止";
+                    timer1.Start();
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                buttonStart.Text = "開始";
+                video?.Dispose();
+                video = null;
+            }
+        }
+
+        object _lock = new object();
+        private void ButtonCapture_Click(object sender, EventArgs e)
+        {
+            using (var bmp = GetCapture())
+            {
+                var cnt = counter++;
+                var filename = $"{textBox1.Text}_{cnt.ToString("D4")}.jpg";
+                bmp.Save(filename, ImageFormat.Jpeg);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (null == video) return;
+            var bmp = GetCapture();
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = bmp;
+            timer1.Start();
+        }
+
+        Bitmap GetCapture()
+        {
+            lock (_lock)
+            {
+                using (var mat = new Mat())
+                {
+                    video.Read(mat);
+                    return mat.ToBitmap();
+                }
+            }
+        }
+    }
+}
